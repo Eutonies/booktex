6b26b21c33e59615313c2c5c48101066b9fff925
Sune Rønne <SURN@nykredit.dk>
2025-03-29 13:43:28 +0100

    Much bettah

---
+++ Booktex.Html/Story/StorySingingComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿using Booktex.Domain.Book.Model;
+using Booktex.Domain.Util;
+using Microsoft.AspNetCore.Components;
+
+namespace Booktex.Html.Story;
+
+public partial class StorySingingComponent
+{
+    [Parameter]
+    public BookSinging Singing { get; set; }
+
+    private IReadOnlyCollection<(string Line, int Index)> AllLines => Singing
+        .LinesSong
+        .Select((_,indx) => (Line: _, Index: indx))
+        .ToReadonlyCollection(); 
+
+}
