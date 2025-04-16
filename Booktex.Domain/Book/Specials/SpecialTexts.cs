581c1fc24b19228bf8861194f5b1e7ea59cdfc3f
Sune Rønne <SURN@nykredit.dk>
2025-04-16 21:44:08 +0200

    Les' roll dem r's

---
+++ Booktex.Domain/Book/Specials/SpecialTexts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booktex.Domain.Book.Specials;
+public static class SpecialTexts
+{
+    public const string FastRollingR = "|||r|||";
+    public const string SlowRollingR = "||r||";
+
+
+}
--- Booktex.Html/Story/StoryCharacterLineComponent.razor.cs
+++ Booktex.Html/Story/StoryCharacterLineComponent.razor.cs
@@ -1,11 +1,15 @@
 ﻿using Booktex.Domain.Book.Model;
+using Booktex.Domain.Book.Specials;
 using Booktex.Domain.Util;
 using Microsoft.AspNetCore.Components;
+using System.Text;
 
 namespace Booktex.Html.Story;
 
 public partial class StoryCharacterLineComponent
 {
+    
+
     [Parameter]
     public BookCharacterLine Line { get; set; }
 
@@ -22,5 +26,27 @@ public partial class StoryCharacterLineComponent
         BookInteractionType.SMS => "int-sms",
         _ => null
     };
+
+    private static MarkupString TextFor(BookCharacterLinePart part)
+    {
+        if(part.PartText.Contains(SpecialTexts.FastRollingR) || part.PartText.Contains(SpecialTexts.SlowRollingR))
+        {
+            return ProduceRollingRs(part);
+        }
+        return new MarkupString(part.PartText);
+    }
+
+    private static MarkupString ProduceRollingRs(BookCharacterLinePart part)
+    {
+        var replacedText = part.PartText
+            .Replace(SpecialTexts.FastRollingR, FastRollingR)
+            .Replace(SpecialTexts.SlowRollingR, SlowRollingR);
+        var returnee = $@"<div class=""animation-text"">{replacedText}</div>";
+        return new MarkupString(returnee);
+    }
+
+
+    private static readonly string FastRollingR = @"<div class=""rolling-r""><div class=""rolling-r-fast"">r</div></div>";
+    private static readonly string SlowRollingR = @"<div class=""rolling-r""><div class=""rolling-r-slow"">r</div></div>";
 
 }
