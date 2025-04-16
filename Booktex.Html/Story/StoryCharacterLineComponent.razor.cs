adfabab9efc99d27e481298f99046f73ac20fe1f
Sune Rønne <SURN@nykredit.dk>
2025-04-16 23:31:11 +0200

    With isolation ID

--- Booktex.Html/Story/StoryCharacterLineComponent.razor.cs
+++ Booktex.Html/Story/StoryCharacterLineComponent.razor.cs
@@ -41,12 +41,12 @@ public partial class StoryCharacterLineComponent
         var replacedText = part.PartText
             .Replace(SpecialTexts.FastRollingR, FastRollingR)
             .Replace(SpecialTexts.SlowRollingR, SlowRollingR);
-        var returnee = $@"<div class=""animation-text"">{replacedText}</div>";
+        var returnee = $@"<div class=""animation-text"" {IsolationId}>{replacedText}</div>";
         return new MarkupString(returnee);
     }
 
-
-    private static readonly string FastRollingR = @"<div class=""rolling-r""><div class=""rolling-r-fast"">r</div></div>";
-    private static readonly string SlowRollingR = @"<div class=""rolling-r""><div class=""rolling-r-slow"">r</div></div>";
+    private const string IsolationId = "story-character-line";
+    private static readonly string FastRollingR = $@"<div class=""rolling-r"" {IsolationId}><div class=""rolling-r-fast"" {IsolationId}>r</div></div>";
+    private static readonly string SlowRollingR = $@"<div class=""rolling-r"" {IsolationId}><div class=""rolling-r-slow"" {IsolationId}>r</div></div>";
 
 }
